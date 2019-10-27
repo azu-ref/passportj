04d287279984134f06070cb7a24776eca4dbddc0
fernando azuaje <fjah17@gmail.com>
2019-10-27 03:01:24 -0400

    implementando la ruta sing-provider que permite autenticar usuarios usando api de otros servicios como google

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const UsersService = require('../services/users')
 const validationHandler = require('../utils/middlewares/validationHandler')
 const joi = require('@hapi/joi')
 
-const { createUserSchema } = require('../utils/schemas/users')
+const { createUserSchema, createProviderUserSchema } = require('../utils/schemas/users')
 
 const { config } = require('../config/index')
 
@@ -98,8 +98,45 @@ function authApi(app) {
       } catch (error) {
         next(error)
       }
-    }
-    )
+    })
+
+    router.post('/sign-provider',
+    validationHandler(joi.object(createProviderUserSchema)),
+    async function(req, res, next) {
+        const { body } = req;
+
+      const { apiKeyToken, ...user } = body;
+
+      if (!apiKeyToken) {
+        next(boom.unauthorized('apiKeyToken is required'));
+      }
+
+      try {
+        const queriedUser = await userService.getOrCreateUser(user);
+        const apiKey = await apiKeysService.getApiKeys({ token: apiKeyToken });
+
+        if (!apiKey) {
+          next(boom.unauthorized());
+        }
+
+        const { _id: id, name, email } = queriedUser;
+
+        const payload = {
+          sub: id,
+          name,
+          email,
+          scopes: apiKey.scopes
+        };
+
+        const token = jwt.sign(payload, config.authJwtSecret, {
+          expiresIn: '15m'
+        });
+
+        return res.status(200).json({ token, user: { id, name, email } });
+      } catch (error) {
+        next(error);
+      }
+    })
 }
 
 module.exports = authApi
